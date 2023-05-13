a553eae07336a6dab979674c6859cf87082d6183
harryalbert <harryalbert364@gmail.com>
2023-05-13 12:50:49 -0400

    added link icon

--- src/components/LinkOffsetText.tsx
+++ src/components/LinkOffsetText.tsx
@@ -14,11 +14,24 @@ export default function LinkOffsetText(props: Props): JSX.Element {
 				props.smallMargin ? "m-1" : "mr-5"
 			}`}
 		>
-			<h1 class="relative z-20 font-mulish text-base tracking-wide">
-				<a href={props.link} target="_blank">
+			<a href={props.link} target="_blank">
+				<h1 class="flex flex-row items-center relative z-20 font-mulish text-base tracking-wide">
 					{props.text}
-				</a>
-			</h1>
+					<svg
+						fill="currentColor"
+						stroke-width="0"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						height="1em"
+						width="1em"
+						style="overflow: visible;"
+						class="ml-1"
+					>
+						<path d="m13 3 3.293 3.293-7 7 1.414 1.414 7-7L21 11V3z"></path>
+						<path d="M19 19H5V5h7l-2-2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2v-5l-2-2v7z"></path>
+					</svg>
+				</h1>
+			</a>
 			<span class="bg-gradient-to-r from-violet-200 to-blue-200 absolute top-3.5 w-full h-1/5 z-10"></span>
 		</span>
 	);
