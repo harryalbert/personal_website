a3ee7bf78c36745313b3856b36b0ef75bf6df606
harryalbert <harryalbert364@gmail.com>
2023-05-19 11:16:42 -0400

    faded color gradient

--- src/components/TitleOffsetText.tsx
+++ src/components/TitleOffsetText.tsx
@@ -9,14 +9,22 @@ interface Props {
 export default function OffsetText(props: Props): JSX.Element {
 	return (
 		<span class="relative inline-block">
-			<h1
-				class={`relative ${
-					props.isMobile ? "text-3xl" : "text-4xl"
-				} z-20 font-fatface tracking-wide`}
-			>
-				{props.text}
-			</h1>
-			<span class="bg-gradient-to-r from-violet-300 to-blue-300 absolute top-6 w-full h-2/5 z-10"></span>
+			<h1 class="magic-text">{props.text}</h1>
 		</span>
 	);
 }
+
+// export default function OffsetText(props: Props): JSX.Element {
+// 	return (
+// 		<span class="relative inline-block">
+// 			<h1
+// 				class={`relative ${
+// 					props.isMobile ? "text-3xl" : "text-4xl"
+// 				} z-20 font-fatface tracking-wide`}
+// 			>
+// 				{props.text}
+// 			</h1>
+// 			<span class="bg-gradient-to-r from-violet-300 to-blue-300 absolute top-6 w-full h-2/5 z-10"></span>
+// 		</span>
+// 	);
+// }
