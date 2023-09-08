f2f273435124be6b2147031645812a25c055c1b8
harryalbert <harryalbert364@gmail.com>
2023-09-08 10:40:52 -0400

    updated job experience

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,34 +8,42 @@ import OffsetText from "~/components/TitleOffsetText";
 
 const jobExperiences = [
 	{
-		jobName: "Meta",
-		jobTitle: "SWE Intern",
-		jobDates: "05/2022 - 8/2022",
+		jobName: "Bubble",
+		jobTitle: "Software Engineer, Intern",
+		jobDates: "Summer 2023",
 		jobDescription:
-			"At Meta, I created two full-scale projects, learned multiple new frameworks and programming languages, and contributed the second most lines of code in my entire team. My work at Meta mainly involved working within the web codebase to create data visualizations using React and GraphQl.",
+			"Bubble is a no-code web development platform. I worked on the Growth Team, and acted as a full time engineer on the team by completing sprint tickets and contributing to AGILE ceremonies. At Bubble, I ideated and executed a project to refresh new hire onboarding, used by all new hires at Bubble, and created a universal set of standards for writing and maintaining documentation. I also contributed to multiple successful experiments, including improving the editor UI and user journey.",
+		jobLink: "https://bubble.io/",
+	},
+	{
+		jobName: "Meta",
+		jobTitle: "Software Engineer, Intern",
+		jobDates: "Summer 2022",
+		jobDescription:
+			"I worked on the Service Efficiency & Capacity Tooling Team at Meta. During my time at Meta, I created two dynamic data visualization projects used by many engineers outside of the team. I also interviewed Meta engineers and gathered feedback in order to tailor projects to, and advocate for, their needs. I utilized this user feedback to update my project designs, and in the process of creating my project I wrote the second most lines of code of any engineer in the team",
 		jobLink: "https://about.meta.com/",
 	},
 	{
 		jobName: "Cadence Effects",
-		jobTitle: "SWE Intern",
-		jobDates: "07/2020 - 8/2021",
+		jobTitle: "Software Engineer",
+		jobDates: "July 2020 – August 2021",
 		jobDescription:
-			"At Cadence, I used Python to independently create many production and file management tools under the direction of the Head of Production. These tools integrated with existing workflow to streamline the production pipeline and improve file management efficiency. I also created a company intranet using node.js and python. Cadence has worked on shows and movies such as Game Of Thrones, Stranger Things, and The Whale.",
+			"Cadence Effects is a VFX company based in New Lebanon, NY. I Took a gap year to work as a full time software engineer at Cadence. During my gap year, I independently ideated and executed efficiency tools under the direction of the Head of Production. I also streamlined the production pipeline by creating file management tools which integrated with existing workflows, and automated video transferring tasks by creating tools that interacted with the company's web-based video storage platform. Finally, I created and maintained a company intranet",
 		jobLink: "https://cadence-effects.com/",
 	},
 	{
 		jobName: "Williams College",
 		jobTitle: "CS TA",
-		jobDates: "02/2022 - 05/2023",
+		jobDates: "February 2022 – Present",
 		jobDescription:
 			"I have TA'ed for CS 136: Data Structures and Advanced Programming, and CS 371: Graphics. For these classes I helped explain complex programming topics to students and graded assignments.",
 	},
 	{
-		jobName: "Plexus",
+		jobName: "Plexus Notes",
 		jobTitle: "Programming Consultant",
-		jobDates: "11/2021 - 02/2022",
+		jobDates: "November 2021 – February 2022",
 		jobDescription:
-			"I created a custom Chrome Extension for Plexus, and helped them plan their launch within the Williams College community. I also created a set of design notes and critiques for their original website and helped create new design ideas from these critiques. Plexus raised a $1.2m pre-seed backed by a set of investors, including Betaworks, Outlander, and Forum.",
+			"Plexus is a smart note taking startup founded by a Williams College alumnus. I created a custom Chrome Extension for Plexus, and helped them plan their launch within the Williams College community. I also created a set of design notes and critiques for their original website and helped create new design ideas from these critiques. Plexus raised a $1.2m pre-seed backed by a set of investors, including Betaworks, Outlander, and Forum.",
 		jobLink: "https://plexus.earth/",
 	},
 ];
